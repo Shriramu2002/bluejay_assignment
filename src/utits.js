71bb1ed61dfa3716e3a172a40dad64ccf9b1e7ea
Shriramu2002 <76653196+Shriramu2002@users.noreply.github.com>
2023-10-01 08:08:31 +0530

    Update utits.js

--- src/utits.js
+++ src/utits.js
@@ -1,3 +1,7 @@
+/** This function takes the difference between 'Time Out' column in the previous entry
+    and 'Time' column in the current entry (same employee), to check if the difference 
+    lies between 1 hour and 10 hours.
+**/
 export function freetimeconst(freetime)
 {
     const llimit = 1 * 60 * 60 * 1000;
@@ -5,6 +9,10 @@ export function freetimeconst(freetime)
     return (freetime>=llimit && freetime<=ulimit);
 }
 
+/** This function takes the 'Time Out' column in the last entry and 'Time'
+    column in the current entry, and check if they are of the same date,
+    or 1 day after, or more.
+**/
 export function consConst(currDay, lastday)
 {
     currDay = currDay.split(' ')[0];
@@ -26,6 +34,9 @@ export function consConst(currDay, lastday)
 
 }
 
+/** This function checks the 'Timecard Hours' column values for each shift entry
+    of the employee to see if any of its value is greater than 14 hours.
+**/
 export function shifttimeconst(shifthours)
 {
     shifthours = shifthours.split(':')[0];
